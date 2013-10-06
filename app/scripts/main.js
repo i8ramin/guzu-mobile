f87fc3a87424a52fb86527fb120a8a4c96987ef7
Ramin B <i8ramin@gmail.com>
2013-10-06 15:09:22 +0330

    appcache

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 (function ($) {
   var touchEvent = Modernizr.touch ? 'tap' : 'click';
-  var scrolled = false;
+  var scrolling = false;
+  var activeElem = false;
+  var tapTimer;
 
   // var snapper = new Snap({
   //   element: $('#main')[0],
@@ -14,13 +16,66 @@
   // });
 
   $(document).on('touchstart', '[tap]', function (e) {
-    $(this).addClass('tapped');
+    var tapElem = $(this);
+
+    tapTimer = setTimeout(function () {
+      tapElem.addClass('tapped');
+    }, 100);
   });
+
   $(document).on('touchend', '[tap]', function (e) {
-    $(this).removeClass('tapped').toggleClass('active');
+    e.preventDefault();
+
+    var tapElem = $(this);
+    var isLink = tapElem.is('a');
+
+    clearTimeout(tapTimer);
+
+    if (!tapElem.hasClass('tapped') && !scrolling) {
+      tapElem.addClass('tapped');
+    }
+
+    tapTimer = setTimeout(function () {
+      tapElem.removeClass('tapped');
+    }, 150);
+
+    if (!scrolling) {
+      if (isLink) {
+        location.href = tapElem.attr('href');
+        return;
+      }
+
+      if (activeElem && (activeElem[0] === tapElem[0])) {
+        activeElem.toggleClass('active');
+        activeElem.css("z-index", 1); // force re-flow
+      } else {
+        if (activeElem) {
+          activeElem.removeClass('active');
+          activeElem = false;
+        }
+
+        activeElem = tapElem;
+        activeElem.addClass('active');
+        activeElem.css("z-index", 1); // force re-flow
+      }
+    }
   });
-  $(document).on('touchstart', function (e) {
-    $(this).find('[tap]').removeClass('active');
+
+  $(document).on('touchmove', '[tap]', function (e) {
+    var tapElem = $(this);
+    tapElem.removeClass('tapped');
+  });
+
+  $(document).on('click', '[tap]', function (e) {
+    e.preventDefault();
+  });
+
+  // scroll detection
+  $(document).on('touchmove', function (e) {
+    scrolling = true;
+  });
+  $(window).on('scroll', function (e) {
+    scrolling = false;
   });
 
   // $(document).on(touchEvent, '[tap]', function (e) {
